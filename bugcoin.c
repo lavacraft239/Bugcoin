4b536e4dd2f8f42942466bed82142891bbdc283a
Iru2000 <camilagoyechea790@gmail.com>
2025-08-24 20:48:14 -0300

    Bugcoin code

--- bugcoin.c
+++ bugcoin.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
 
 typedef struct {
     int index;
@@ -72,8 +73,31 @@ Block createBlock(int index, int prevHash, char *data) {
     return b;
 }
 
+typedef struct {
+    int startIndex;
+    int lastHash;
+    int duration;
+} ThreadData;
+
+void *mineThread(void *arg) {
+    ThreadData *td = (ThreadData *)arg;
+    time_t start = time(NULL);
+    int index = td->startIndex;
+    int lastHash = td->lastHash;
+    while (time(NULL) - start < td->duration) {
+        char data[256];
+        sprintf(data, "Block mined at %ld by thread %lu", time(NULL), pthread_self());
+        Block b = createBlock(index, lastHash, data);
+        lastHash = b.hash;
+        index++;
+    }
+    return NULL;
+}
+
 int main(int argc, char *argv[]) {
     int nodeOn = 0, mining = 0, duration = 0;
+    int threads = 2;
+
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--true") == 0) nodeOn = 1;
         if (strcmp(argv[i], "--false") == 0) nodeOn = 0;
@@ -88,25 +112,32 @@ int main(int argc, char *argv[]) {
             if (unit == 'm') duration = value * 60;
             i++;
         }
+        if (strcmp(argv[i], "--threads") == 0 && i + 1 < argc) {
+            threads = atoi(argv[i + 1]);
+            i++;
+        }
     }
+
     if (nodeOn) printf("Bugcoin node on\n");
     else {
         printf("Bugcoin node off\n");
         return 0;
     }
+
     if (mining) {
-        printf("Mining Bugcoin for %d seconds...\n", duration);
-        time_t start = time(NULL);
+        printf("Mining Bugcoin for %d seconds using %d threads...\n", duration, threads);
+        pthread_t tid[threads];
         int lastHash = getLastHash();
-        int index = 1;
-        while (time(NULL) - start < duration) {
-            char data[256];
-            sprintf(data, "Block mined at %ld", time(NULL));
-            Block b = createBlock(index, lastHash, data);
-            lastHash = b.hash;
-            index++;
+        for (int i = 0; i < threads; i++) {
+            ThreadData *td = malloc(sizeof(ThreadData));
+            td->startIndex = i + 1;
+            td->lastHash = lastHash;
+            td->duration = duration;
+            pthread_create(&tid[i], NULL, mineThread, td);
         }
+        for (int i = 0; i < threads; i++) pthread_join(tid[i], NULL);
         printf("Mining finished\n");
     }
+
     return 0;
 }
